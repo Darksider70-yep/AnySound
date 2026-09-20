a2daddf0ca590e416d72f56bfcbc33813a4dba45
Darksider70-yep <darksider70117@gmail.com>
2026-09-21 02:45:01 +0530

    feat: implement JNI bridge for Android native app controller interface

--- apps/android/app/src/main/cpp/chorus_jni.cpp
+++ apps/android/app/src/main/cpp/chorus_jni.cpp
@@ -1,6 +1,8 @@
 #include <chorus/app/app_controller.hpp>
 #include <nlohmann/json.hpp>
 
+#include <string>
+
 #if defined(__has_include)
 #if __has_include(<jni.h>)
 #include <jni.h>
@@ -12,37 +14,34 @@ using jboolean = unsigned char;
 using jfloat = float;
 using jobject = void*;
 using jstring = void*;
-#define JNI_FALSE 0
-#define JNI_TRUE 1
+inline constexpr jboolean JNI_FALSE = 0;
+inline constexpr jboolean JNI_TRUE = 1;
 #define JNIEXPORT
 #define JNICALL
 
 struct JNIEnv {
-    const char* GetStringUTFChars(jstring /*str*/, jboolean* /*isCopy*/) { return nullptr; }
-    void ReleaseStringUTFChars(jstring /*str*/, const char* /*chars*/) {}
-    jstring NewStringUTF(const char* /*bytes*/) { return nullptr; }
+    static const char* GetStringUTFChars(jstring /*str*/, jboolean* /*isCopy*/) { return nullptr; }
+    static void ReleaseStringUTFChars(jstring /*str*/, const char* /*chars*/) {}
+    static jstring NewStringUTF(const char* /*bytes*/) { return nullptr; }
 };
 #endif
 
 #if __has_include(<android/log.h>)
 #include <android/log.h>
-#define TAG "ChorusJNI"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "ChorusJNI", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "ChorusJNI", __VA_ARGS__)
 #else
-#define TAG "ChorusJNI"
-#define LOGI(...) ((void)0)
-#define LOGE(...) ((void)0)
+template <typename... Args>
+inline void LOGI(Args&&...) {}
+template <typename... Args>
+inline void LOGE(Args&&...) {}
 #endif
 #else
 #include <android/log.h>
 #include <jni.h>
-#define TAG "ChorusJNI"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, TAG, __VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, "ChorusJNI", __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, "ChorusJNI", __VA_ARGS__)
 #endif
-
-#include <string>
 
 namespace {
 
@@ -51,11 +50,11 @@ chorus::AppController* get_controller(jlong handle) {
 }
 
 std::string jstring_to_utf8(JNIEnv* env, jstring jstr) {
-    if (!jstr) {
+    if (jstr == nullptr) {
         return "";
     }
     const char* chars = env->GetStringUTFChars(jstr, nullptr);
-    if (!chars) {
+    if (chars == nullptr) {
         return "";
     }
     std::string result(chars);
@@ -85,7 +84,9 @@ JNIEXPORT jboolean JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeStartHost(
     JNIEnv* env, jobject /*thiz*/, jlong handle, jstring pin, jlong target_latency_ms, jboolean use_test_tone) {
     auto* controller = get_controller(handle);
-    if (!controller) return JNI_FALSE;
+    if (controller == nullptr) {
+        return JNI_FALSE;
+    }
     std::string pin_str = jstring_to_utf8(env, pin);
     bool ok = controller->start_host(pin_str, static_cast<uint64_t>(target_latency_ms), use_test_tone == JNI_TRUE);
     return ok ? JNI_TRUE : JNI_FALSE;
@@ -94,7 +95,7 @@ Java_dev_chorus_app_service_NativeChorusBridge_nativeStartHost(
 JNIEXPORT void JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeStopHost(JNIEnv* /*env*/, jobject /*thiz*/, jlong handle) {
     auto* controller = get_controller(handle);
-    if (controller) {
+    if (controller != nullptr) {
         controller->stop_host();
     }
 }
@@ -103,7 +104,9 @@ JNIEXPORT jboolean JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeJoinHost(
     JNIEnv* env, jobject /*thiz*/, jlong handle, jstring host_ip, jint port, jstring pin) {
     auto* controller = get_controller(handle);
-    if (!controller) return JNI_FALSE;
+    if (controller == nullptr) {
+        return JNI_FALSE;
+    }
     std::string ip_str = jstring_to_utf8(env, host_ip);
     std::string pin_str = jstring_to_utf8(env, pin);
     bool ok = controller->join_host(ip_str, static_cast<uint16_t>(port), pin_str);
@@ -113,7 +116,7 @@ Java_dev_chorus_app_service_NativeChorusBridge_nativeJoinHost(
 JNIEXPORT void JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeLeaveHost(JNIEnv* /*env*/, jobject /*thiz*/, jlong handle) {
     auto* controller = get_controller(handle);
-    if (controller) {
+    if (controller != nullptr) {
         controller->leave_host();
     }
 }
@@ -121,7 +124,7 @@ Java_dev_chorus_app_service_NativeChorusBridge_nativeLeaveHost(JNIEnv* /*env*/, 
 JNIEXPORT void JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeUpdate(JNIEnv* /*env*/, jobject /*thiz*/, jlong handle) {
     auto* controller = get_controller(handle);
-    if (controller) {
+    if (controller != nullptr) {
         controller->update();
     }
 }
@@ -130,7 +133,7 @@ JNIEXPORT void JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeSetVolume(
     JNIEnv* /*env*/, jobject /*thiz*/, jlong handle, jfloat volume) {
     auto* controller = get_controller(handle);
-    if (controller) {
+    if (controller != nullptr) {
         controller->set_volume(volume);
     }
 }
@@ -139,7 +142,7 @@ JNIEXPORT void JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeSetMute(
     JNIEnv* /*env*/, jobject /*thiz*/, jlong handle, jboolean mute) {
     auto* controller = get_controller(handle);
-    if (controller) {
+    if (controller != nullptr) {
         controller->set_mute(mute == JNI_TRUE);
     }
 }
@@ -148,7 +151,7 @@ JNIEXPORT void JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeSetOffsetMs(
     JNIEnv* /*env*/, jobject /*thiz*/, jlong handle, jint offset_ms) {
     auto* controller = get_controller(handle);
-    if (controller) {
+    if (controller != nullptr) {
         controller->set_offset_ms(offset_ms);
     }
 }
@@ -157,7 +160,7 @@ JNIEXPORT jstring JNICALL
 Java_dev_chorus_app_service_NativeChorusBridge_nativeGetSnapshotJson(
     JNIEnv* env, jobject /*thiz*/, jlong handle) {
     auto* controller = get_controller(handle);
-    if (!controller) {
+    if (controller == nullptr) {
         return env->NewStringUTF("{}");
     }
 
@@ -184,7 +187,7 @@ Java_dev_chorus_app_service_NativeChorusBridge_nativeGetSnapshotJson(
         {"sync_error_us", snap.client_stats.sync_error_us},
         {"skew_ppm", snap.client_stats.skew_ppm},
         {"underruns", snap.client_stats.underruns},
-        {"late_frames", snap.client_stats.late_frames},
+        {"late_frames", snap.client_stats.late},
         {"loss_pct", snap.client_stats.loss_pct},
         {"buffer_ms", snap.client_stats.buffer_ms}
     };
@@ -193,13 +196,13 @@ Java_dev_chorus_app_service_NativeChorusBridge_nativeGetSnapshotJson(
     auto clients_arr = nlohmann::json::array();
     for (const auto& c : snap.connected_clients) {
         clients_arr.push_back({
-            {"client_id", c.client_id},
+            {"client_id", c.id},
             {"name", c.name},
-            {"endpoint", c.endpoint_str},
+            {"endpoint", c.address + ":" + std::to_string(c.control_port)},
             {"volume", c.volume},
             {"is_muted", c.is_muted},
             {"offset_ms", c.offset_ms},
-            {"sync_error_us", c.sync_error_us}
+            {"sync_error_us", c.last_stats.sync_error_us}
         });
     }
     j["connected_clients"] = clients_arr;
@@ -208,10 +211,10 @@ Java_dev_chorus_app_service_NativeChorusBridge_nativeGetSnapshotJson(
     auto hosts_arr = nlohmann::json::array();
     for (const auto& h : snap.discovered_hosts) {
         hosts_arr.push_back({
-            {"name", h.session_name},
-            {"ip", h.host_ip},
-            {"control_port", h.control_port},
-            {"audio_port", h.audio_port}
+            {"name", h.name},
+            {"ip", h.address},
+            {"control_port", h.tcp_port},
+            {"audio_port", h.udp_port}
         });
     }
     j["discovered_hosts"] = hosts_arr;
